d7d80c8327152baab611172fb1b3e475a6ec891e
Kevin Candelaria <kevdashdev@gmail.com>
2013-04-25 21:53:02 +0800

    Explicitly state config module location with __dirname in server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var express       = require('express')
   , mongoose      = require('mongoose');
 
 var app           = express()
-  , config        = require('./apps/config.coffee')(app, express, mongoose)
+  , config        = require(__dirname + '/apps/config')(app, express, mongoose)
   , models        = {};
 
 var server = http.createServer(app).listen(app.get('port'), function(){
